6a9b534ef1dbafd052492f616f5f01524512383a
Wei (Wayne) Hu <wei.hu@duke.edu>
2020-11-02 05:13:30 -0500

    Add cluster points

--- sta_dashboard/routes.py
+++ sta_dashboard/routes.py
@@ -11,12 +11,13 @@ def index():
         locations = []
         for endpoint in endpoints:
             locations.extend(
-                Location.query.filter(Location.endpoint == endpoint).all()
+                Location.query.filter(
+                    Location.endpoint == endpoint).limit(500).all()
                 )
             
         zoom_level = 3 if len(endpoints) > 1 else 5
         return render_template('index.html', locations=locations, zoom_level=zoom_level)
     
     locations = Location.query.filter(
-        Location.endpoint == 'internetofwater').all()
+        Location.endpoint == 'internetofwater').limit(500).all()
     return render_template('index.html', locations=locations, zoom_level=5)
