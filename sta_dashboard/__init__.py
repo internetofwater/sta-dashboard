75148eb4a0e2de6dd895aa7a8da37b080a035073
Wei (Wayne) Hu <wei.hu@duke.edu>
2021-06-14 09:43:21 -0400

    Tidy __init__.py

--- sta_dashboard/__init__.py
+++ sta_dashboard/__init__.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 user = os.environ["POSTGRES_USER"]
 password = os.environ["POSTGRES_PASSWORD"]
-port = os.environ["POSTGRES_HOST_PORT"]
 database = os.environ["POSTGRES_DB"] 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    f'postgresql+psycopg2://{user}:{password}@postgres:{port}/{database}'
+    f'postgresql+psycopg2://{user}:{password}@postgres:5432/{database}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
