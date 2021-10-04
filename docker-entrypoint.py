0256fa39aa604dfca3dde39a9af7671a6a969042
Wei (Wayne) Hu <wei.hu@duke.edu>
2021-10-04 10:10:49 -0400

    Add docker vollume support and update README.md

--- docker-entrypoint.py
+++ docker-entrypoint.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-if not os.path.exists(os.path.join('sta_dashboard', os.environ["SQLITE_DB_FILENAME"])):
+if not os.path.exists(os.path.join('sta_dashboard', 'data', os.environ["SQLITE_DB_FILENAME"])):
     subprocess.run(['python', 'db_cache.py'])
     
 subprocess.run(['python', 'app.py'])
--- sta_dashboard/__init__.py
+++ sta_dashboard/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.environ["SQLITE_DB_FILENAME"]}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///data/{os.environ["SQLITE_DB_FILENAME"]}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
